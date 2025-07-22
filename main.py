2f956de93ea357b0d7d83ea1f5abdc049e2409fc
DevDocXIII <dshelfoon@gmail.com>
2025-07-22 13:40:07 -0400

    rasin frasin code completin

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from stats import get_num_words, get_number_of_each_character
 
 
 def main():
+    print ("============ BOOKBOT ============")
+    print ("Analyzing book found at books/frankenstein.txt...")
+    print ("----------- Word Count ----------")
     get_num_words("./books/frankenstein.txt")
+    
     get_number_of_each_character("./books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -6,7 +6,8 @@ def get_num_words(path_to_file):
         for word in words:
             word_count += 1
     #print(words)
-    print(f"{word_count} words found in the document")
+    print ("Found "f"{word_count}" " total words")
+    print ("--------- Character Count -------")
 
 
 def get_number_of_each_character(path_to_file):
@@ -22,4 +23,29 @@ def get_number_of_each_character(path_to_file):
                         character_counts[char] += 1
                     else:
                         character_counts[char] = 1
-        print(character_counts)
+    sort_by_count(character_counts)
+
+def sort_on(items):
+    return items["num"]
+
+def sort_by_count(character_counts):
+    char_stats = []
+                #   "char":"",
+                #   "num": 0
+                #   }
+    #print (f"{character_counts}" )
+#    char_list = [{"char": char, "num": count} for char, count in character_counts.items()]
+#    char_list.sort(key=lambda x: x["num"], reverse=True)
+    for character in character_counts:
+        #print(character)
+        char_stats.append({"char": character,"num": character_counts[character]})
+        #            }
+        # char_stats = [
+        #              {"char": character,"num": character_counts[character]
+        #               }
+        #              ]
+#        char_stats["num"] 
+    char_stats.sort(reverse=True,key=sort_on)
+    for result in char_stats:
+        print (f"{result["char"]}: {result["num"]}" )
+
