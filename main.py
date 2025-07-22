056d90b24fb5e67f46fd9b7125d46af0138e5ee5
DevDocXIII <dshelfoon@gmail.com>
2025-07-22 17:15:56 -0400

    finally done with this

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from sys import argv, exit
 from stats import get_num_words, get_number_of_each_character
-
-
+if len(argv) < 2: 
+    print ("Usage: python3 main.py <path_to_book>")
+    print(f"{argv} argument provided")
+    exit(1)
+print(f"{argv} argument provided")
 def main():
     print ("============ BOOKBOT ============")
-    print ("Analyzing book found at books/frankenstein.txt...")
+    print ("Analyzing book found at " f"{argv[1]}...")# books/frankenstein.txt...")
     print ("----------- Word Count ----------")
-    get_num_words("./books/frankenstein.txt")
+#    get_num_words("./books/frankenstein.txt")
+    get_num_words(argv[1])
     
-    get_number_of_each_character("./books/frankenstein.txt")
+    get_number_of_each_character(argv[1])
 
 main()
