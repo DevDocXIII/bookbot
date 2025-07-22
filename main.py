019b947a4ccc85d4af7ff00df4922e248012d3ea
DevDocXIII <dshelfoon@gmail.com>
2025-07-22 10:39:45 -0400

    Added get_number_of_words

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from stats import get_num_words
+from stats import get_num_words, get_number_of_each_character
+
 
 def main():
-    get_num_words("./books/frankenstein.txt")
+    #get_num_words("./books/frankenstein.txt")
+    get_number_of_each_character("./books/frankenstein.txt")
 
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,19 @@ def get_num_words(path_to_file):
             word_count += 1
     #print(words)
     print(f"{word_count} words found in the document")
+
+
+def get_number_of_each_character(path_to_file):
+    character_counts = {}
+    with open(path_to_file) as file:
+        file_contents = file.read()
+        words = file_contents.split()
+        for word in words:
+            for char in word:
+                char = char.lower()
+                if char.isalph:
+                    if char in character_counts:
+                        character_counts[char] += 1
+                    else:
+                        character_counts[char] = 1
+        print(character_counts)
